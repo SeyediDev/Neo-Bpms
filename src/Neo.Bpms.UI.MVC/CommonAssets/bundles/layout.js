c7f0d075e265927e11170ba51c050718e916b185
javadsayedi <javadsayedi@gmail.com>
2025-12-03 12:40:09 +0330

    .

--- src/Neo.Bpms.UI.MVC/CommonAssets/bundles/layout.js
+++ src/Neo.Bpms.UI.MVC/CommonAssets/bundles/layout.js
@@ -1223,12 +1223,6 @@ var eTargetArea =
 };
 
 function Client_AddClass(This, scope, fieldName, className, targetArea, formula) {
-    alert("This:" + This +
-        "\nscope:" + scope +
-        "\nfieldName:" + fieldName +
-        "\nclassName:" + className +
-        "\ntargetArea:" + targetArea +
-        "\nformula:" + formula);
 	if (scope) {
 		var rowIndex = $(This).closest("tr").index();
 		var table = $('table[name="' + scope + '"]');
