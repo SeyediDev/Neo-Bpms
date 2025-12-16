d93a5117286642415734ba5897e35645aaebeaff
javadsayedi <javadsayedi@gmail.com>
2025-12-16 19:08:50 +0330

    Update ReportTotalRecord.cs

--- src/Neo.Bpms.Infrastructure/Features/Cmmn/Reports/ReportTotalRecord.cs
+++ src/Neo.Bpms.Infrastructure/Features/Cmmn/Reports/ReportTotalRecord.cs
@@ -48,7 +48,7 @@ public static class ReportTotalRecord
             result.RecordCount = (int)qCount.GetRecordCount(result.Structure.FilterValues, lp);
             if (result.RecordCount == 0 && !string.IsNullOrEmpty(qCount.ErrorText))
             {
-                //result.Errors.AddError(qCount.ErrorText, "Query", "13.0.2", qCount.ErrorText);
+                //TODO Temp : result.Errors.AddError(qCount.ErrorText, "Query", "13.0.2", qCount.ErrorText);
             }
         }
         catch (Exception e)
