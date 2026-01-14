399f139cc56019595d6f76ed623d3fd9890a5843
javadsayedi <javadsayedi@gmail.com>
2026-01-14 10:05:45 +0330

    .

--- src/Neo.Bpms.Api/GlobalUsings.cs
+++ src/Neo.Bpms.Api/GlobalUsings.cs
@@ -1,14 +1,8 @@
-global using System.Collections.Concurrent;
-global using System.Diagnostics;
-global using System.Diagnostics.Metrics;
 global using Microsoft.AspNetCore.Builder;
 global using Microsoft.AspNetCore.Http;
 global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.AspNetCore.Routing;
-global using Microsoft.AspNetCore.SignalR;
 global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection;
-global using Microsoft.Extensions.Hosting;
 global using Microsoft.Extensions.Logging;
-global using Microsoft.Extensions.Options;
 
